11122cf1b8e70f1f2d5a9864629bb597820452c5
gaby <66489618+g308518@users.noreply.github.com>
2023-02-04 21:11:08 -0600

    initiated the python file with pseudocode
    just created the python file with vague pseudo code for this week's project

---
+++ project3.py
@@ -0,0 +1,18 @@
+#
+# for this week's group project we must use the provided apache log file and answer the following questions:
+# 1. how many total requests have been made in the 6 months?
+# 2. how many total requests were made in the time period represented by the log
+
+
+# 1. starting off with pseudocode, here is what we must do:
+# parse the log file so that we can store the month as a variable "month"
+# set a counter variable to 1 so int count = 0
+# if the variable "month" is greater than 6 or == 1
+# count += 1
+# uncertainties: will we need a while loop to iterate through each line?
+
+# 2. some pseudocode to start:
+# set counter variable to 0
+# create while loop to iterate through log file by line
+# increase counter variable by 1 each iteration
+# uncertainties: how will the loop know when to end?
